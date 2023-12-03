1b76a8a3ebae8227655c409b404ac59795218591
HarunaZakaria <Harunzy55@gmail.com>
2023-12-03 12:09:17 +0000

    Create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> Trinity Collection</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
