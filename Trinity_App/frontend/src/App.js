eec302e5d1034df1897cac2b9985078430f1e74b
harunazakaria <Harunzy55@gmail.com.com>
2023-12-29 01:23:42 +0800

    add frontend

---
+++ Trinity_App/frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">CenturyPlus</a>
+      </header>
+      <main>
+        <h1>List Products</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
