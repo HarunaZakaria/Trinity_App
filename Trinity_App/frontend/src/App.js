32025052d393d4d1e0d332b8ee53f13323d33986
harunazakaria <Harunzy55@gmail.com.com>
2023-12-29 09:05:21 +0800

    add product data

--- Trinity_App/frontend/src/App.js
+++ Trinity_App/frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from './data';
+
 function App() {
   return (
     <div>
@@ -5,7 +7,25 @@ function App() {
         <a href="/">CenturyPlus</a>
       </header>
       <main>
-        <h1>List Products</h1>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>Ghc{product.price}</strong>
+                </p>
+                <button>Add to card</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
